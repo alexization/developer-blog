9d4595b32f64ad49171f494222ba99fee19bd6f9
hyoseok <alexization@gmail.com>
2023-11-21 00:36:39 +0900

    Feat: MainPage 레이아웃 구성

--- yonsei/static/main.js
+++ yonsei/static/main.js
@@ -1,6 +1,6 @@
 const toggleBtn = document.querySelector(".navbar_toggleBtn");
 const menu = document.querySelector(".head-menu");
-const link = document.querySelector(".head-ling");
+const link = document.querySelector(".head-link");
 
 toggleBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
