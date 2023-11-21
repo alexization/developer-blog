50c796b5c532281fa4165f8c22df414174848af3
hyoseok <alexization@gmail.com>
2023-11-21 21:10:24 +0900

    Feat: 1차 DB 테이블 구성 및 등록

--- yonsei/mypage/admin.py
+++ yonsei/mypage/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User_Info
+from .models import Post
+from .models import Category
+from .models import Comments
 
 # Register your models here.
+
+admin.site.register(User_Info)
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Comments)
--- yonsei/mypage/models.py
+++ yonsei/mypage/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class User_Info(models.Model):
+    u_id = models.CharField(max_length=20)
+    u_pw = models.CharField(max_length=20)
+    u_name = models.CharField(max_length=10)
+    u_school = models.CharField(max_length=20)
+
+class Post(models.Model):
+    p_title = models.CharField(max_length=50)
+    p_desc = models.CharField(max_length=100)
+    p_contents = models.TextField()
+    p_created = models.DateTimeField(auto_now_add=True)
+    p_updated = models.DateTimeField(auto_now=True)
+    p_secure = models.BooleanField(default=True)
+
+class Category(models.Model):
+    p_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    cate_name = models.CharField(max_length=50)
+
+class Comments(models.Model):
+    p_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    c_user_id = models.CharField(max_length=20)
+    c_user_pw = models.CharField(max_length=20)
+    c_contents = models.TextField()
+    c_created = models.DateTimeField(auto_now_add=True)
+    c_updated = models.DateTimeField(auto_now=True)
