a4a2c82b343ea8d6230b3b83560f95e7da6f34d3
hyoseok <alexization@gmail.com>
2023-11-28 14:03:08 +0900

    Feat: Post Write Page 기능 구현

--- yonsei/mypage/views.py
+++ yonsei/mypage/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Count
+from django.utils import timezone
 from .models import Category
 from .models import Post
 from .models import Comments
@@ -7,7 +8,7 @@ from .models import User_Info
 
 # Create your views here.
 def main(request):
-    post_list = Post.objects.all()
+    post_list = Post.objects.order_by('-id')
     categorys = Category.objects.annotate(post_count=Count('post'))
 
     context = {'post_list' : post_list, 'categorys': categorys}
@@ -30,13 +31,46 @@ def category(request, cate_name):
 
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
+    if request.method == "POST":
+        comment_content = request.POST["comment"]
+        comment_id = request.POST["comment_id"]
+        comment_pw = request.POST["comment_pw"]
 
+        Comments.objects.create(
+            p_id = post_id,
+            c_contents = comment_content,
+            c_user_id = comment_id,
+            c_user_pw = comment_pw,
+            c_created = timezone.now(),
+            c_updated = timezone.now()
+        )
+        print(post_id)
+        
     context = {'post' : post}
     return render(request, 'mypage/detail.html', context)
 
 def post_write(request):
+    category_list = Category.objects.all()
+    context = {'category_list' : category_list}
 
-    return render(request, 'mypage/write.html')
+    if request.method == "POST":
+        
+        cate = request.POST["categorys"]
+        title = request.POST["title"]
+        description = request.POST["description"]    
+        content = request.POST["content"]
+        thumbnail = request.FILES["thumbnail"]
+
+        post = Post.objects.create(
+            cate_id = int(cate),
+            p_title = title,
+            p_desc = description,
+            p_contents = content,
+            thumbnail = thumbnail
+        )
+        return redirect(f"/posts/{post.id}")
+    
+    return render(request, 'mypage/write.html', context)
 
 def about(request):
 
