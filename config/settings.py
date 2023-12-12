ffaac1dc442a8eafbf4ab41fe83c7cc1a5fd66f2
hyoseok <alexization@gmail.com>
2023-12-12 15:38:58 +0900

    nginx configuration update

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 AUTH_USER_MODEL = "mypage.User"
 
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
