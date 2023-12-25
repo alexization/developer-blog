1a50f5ba9b0dcc2abcd4269bbe18c50eaf7e9b27
hyoseok <alexization@gmail.com>
2023-12-25 16:12:58 +0900

    feat: Category 기능 추가

--- mypage/urls.py
+++ mypage/urls.py
@@ -7,13 +7,15 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
     path('write/', views.post_write, name='write'),
     path('about/', views.about, name="about"),
     # views.post_detail를 통해 가져온 post_id 정보를 바탕으로 url 구성
     # posts/1 -> post_id = 1 인 상세페이지로 이동
     path('posts/<int:post_id>/', views.post_detail, name="posts"),
     # 위와 동일
-    path('category/<str:cate_name>/', views.category, name='category'),
+    path('category/', views.category, name='category'),
+    path('category/<str:cate_name>/', views.select_category, name='category'),
     path('option/', views.category_option, name='category option'),
     path('option/delete/<int:category_id>', views.category_delete, name='category delete'),
     path('option/modify/<int:category_id>', views.category_modify, name='category modify'),
